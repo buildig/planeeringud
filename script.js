0ca39ab5f095a888dbd0009b339f47d640f8d2b0
Tormi Tabor <tormi.tabor@gmail.com>
2017-01-29 22:14:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ var info = L.control();
 info.onAdd = function(map) {
     this._div = L.DomUtil.create('div', 'info');
     this._div.innerHTML = (
-        "<h1><a href=\'\/yldplaneering\' title=\'Saue valla üldplaneering\'>Saue valla üldplaneering</a></h1><a href=\'https:\/\/sauevald.ee\' title=\'Saue valla veebileht\'>sauevald.ee</a> | <a href=\'https:\/\/tabor.ee\' title=\'Teostus: Tormi Tabor\' target=\'_blank\'>Teostus: Tormi Tabor</a> | <a href=\'https:\/\/github.com\/sauevald\/yldplaneering\/issues\' title=\'Anna kaardirakendusele tagasisidet\' target=\'_blank\'>Ettepanekud</a>"
+        "<h1><a href=\'\/plans\' title=\'Planeeringud\'>Planeeringud</a></h1> | <a href=\'https:\/\/tabor.ee\' title=\'Teostus: Tormi Tabor\' target=\'_blank\'>Teostus: Tormi Tabor</a> | <a href=\'https:\/\/buildig.com\' title=\'BUILDIG\' target=\'_blank\'>BUILDIG</a>"
     );
     return this._div;
 };
